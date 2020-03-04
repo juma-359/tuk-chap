12950777a79a9b115e420b73e23172a3a4baa04e
juma-359 <joeljuma08@gmail.com>
2020-03-04 16:55:45 +0300

    added the event listeners to allow my page respond on openning the signup side and signin

--- js/login.js
+++ js/login.js
@@ -1,3 +1,11 @@
-$('.message a').click(function(){
-    $('form').animate({height:"toggle", opacity:"toggle"});
-});
+// $('.message a').click(function(){
+//     $('form').animate({height:"toggle", opacity:"toggle"});
+// });
+
+// 
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => container.classList.add('right-panel-active'));
+signInButton.addEventListener('click', () => container.classList.remove('right-panel-active'));
